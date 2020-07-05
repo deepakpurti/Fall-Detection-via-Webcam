a789e506da8ca8ff5893f1a243bc64bebc2aad49
Deepak Purti <31980059+DeepakPurti@users.noreply.github.com>
2020-07-06 04:04:18 +0530

    Update sendemail.m

--- sendemail.m
+++ sendemail.m
@@ -1,9 +1,9 @@
-function sendemail()
+function sendemail() % for Gmail only
 
-mail = 'Sender@gmail.com'; % sender's gmail address
-sender = 'Home Surveillance <Sender@gmail.com>'; 
-receiver = 'Receiver@gmail.com'; %receiver's gmail address
-password = '********';  % my gmail password 
+mail = 'sender@gmail.com'; % sender's gmail address
+sender = 'Sender <sender@gmail.com>'; % "Sender" = sender's name
+receiver = 'receiver@gmail.com'; %receiver's gmail address
+password = '********';  % sender's gmail password 
 host = 'smtp.gmail.com';
 % preferences
 setpref('Internet','SMTP_Server', host);
@@ -15,7 +15,7 @@ props.setProperty('mail.smtp.auth','true');
 props.setProperty('mail.smtp.socketFactory.class', 'javax.net.ssl.SSLSocketFactory');
 props.setProperty('mail.smtp.socketFactory.port','465');
 % Send the email
-sendmail(receiver,'ALERT','Fall Detected!')
+sendmail(receiver,'ALERT','Fall Detected!') % sendemail('receiver's email address','subject of email','message in email')
 
 end
 
