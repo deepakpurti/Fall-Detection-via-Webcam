f5269d4a61132b87da95766fdee6394c2e6e7eac
Deepak Purti <31980059+DeepakPurti@users.noreply.github.com>
2020-07-06 03:43:59 +0530

    Delete modifyMask.m

--- modifyMask.m
+++
@@ -1,8 +0,0 @@
-function fgClose = modifyMask(fgMask,strelType,strelSize)
-
-%finding foregorund mask, filling holes and closing gaps
-
-fgFill = imfill(fgMask,'holes');
-fgClose = imclose(fgFill,strel(strelType,strelSize));
-
-end
